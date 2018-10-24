627225214cb8953b25a2a64bdab8c33d7e1b765f
School <patzila@gmail.com>
2018-10-24 14:13:10 -0400

    Added health to the ui

---
+++ Assets/Character/Scripts/DisplayHP.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DisplayHP : MonoBehaviour {
+
+	// Use this for initialization
+	private Character character;
+	private Text textbox;
+	void Awake () {
+		// Init character controller to get hp
+		character = GameObject.FindGameObjectWithTag("Character").GetComponent<Character>();
+		// Init textbox to display the health
+		textbox = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int hp = character.getHealth();
+		textbox.text = "HP: " + hp;
+	}
+}
