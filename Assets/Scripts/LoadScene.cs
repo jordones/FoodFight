ea4ae446da3286263cb22da46690ce19b6a56c84
David Buckley <buckley.w.david@gmail.com>
2018-10-27 17:52:11 -0500

    Add Firebase SDK and basic MainMenu scene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public string scene = "Devland";
+
+    public void Load()
+    {
+		StartCoroutine(AsyncLoadScene(scene));
+    }
+
+    IEnumerator AsyncLoadScene(string scene)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
