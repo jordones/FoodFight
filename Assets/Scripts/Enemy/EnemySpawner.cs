6510548d60ba5a211b2c5cf82888336b64f08e18
David Buckley <buckley.w.david@gmail.com>
2018-11-14 05:03:37 -0500

    level 3

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -5,19 +5,48 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
 
+    public bool active = true;
     public float spawnTime = 3f;
     public GameObject[] enemyTypes;    // Number of each different enemy type  
     void Start()
     {
-        // Run the Spawn method ever spawn time in 3 seconds every 3 seconds
-        Spawn();
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (active) {
+            StartSpawner();
+        } else {
+            StopSpawner();
+        }
+    }
 
+    void StartSpawner() {
+        // Run the Spawn method every 3 seconds starting now
+        if (!active) {
+            InvokeRepeating("Spawn", 0.0f, spawnTime);
+            active = true;
+        }
+    }
+
+    void StopSpawner() {
+        CancelInvoke();
+        active = false;
+    }
+
+	void OnTriggerEnter2D(Collider2D col) {
+        Debug.Log("Character entred spawner radius");
+		if (col.tag == "Character") {
+            StartSpawner();
+        }
+    }
+
+    void OnTriggerExit2(Collider2D col) {
+        Debug.Log("Character left spawner radius");
+		if (col.tag == "Character") {
+            StopSpawner();
+        }
     }
 
     void Spawn()
