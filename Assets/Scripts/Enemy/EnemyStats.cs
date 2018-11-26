85aab7de0f87fc898aa366e7d5ac32ac1271a8d0
School <patzila@gmail.com>
2018-11-26 18:46:25 -0500

    Fixed spew and slap not working after level transitions

--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyStats : TypedEnemy {
+public class EnemyStats : TypedEnemy
+{
 
     public int maxHealth = 100;
     public int health = 100;
@@ -10,44 +11,52 @@ public class EnemyStats : TypedEnemy {
 
     [SerializeField] private HealthBar healthBar;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         healthBar.SetSize(1f);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (health <= 0) {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
             Die();
         }
     }
 
-    public void TakeDamage (int amount) {
+    public void TakeDamage(int amount)
+    {
         health -= amount;
-        float healthPercent = (float)health/maxHealth;
+        float healthPercent = (float)health / maxHealth;
         healthBar.SetSize(healthPercent);
-        Debug.Log("Enemy damage from " + (health+amount) + " to " + health);
+        Debug.Log("Enemy damage from " + (health + amount) + " to " + health);
     }
 
-    void OnCollisionEnter2D(Collision2D collision) {
+    void OnCollisionEnter2D(Collision2D collision)
+    {
 
         Collider2D collider = collision.collider;
 
-        if (collider.tag == "Character") {
+        if (collider.tag == "Character")
+        {
             collider.gameObject.GetComponent<Character>().TakeDamage(attack);
         }
     }
 
-    private void Die () {
+    private void Die()
+    {
         Debug.Log("Enemy Death");
-        Destroy(gameObject);    
+        Destroy(gameObject);
         LevelManager.instance.Killed(this);
         // print("Enemy destroyed");
     }
 
-    void OnGUI() {
+    void OnGUI()
+    {
         // Vector3 pos = Camera.main.WorldToScreenPoint(gameObject.transform.position);
 
-		// GUI.Label(new Rect (pos.x, Screen.height-pos.y-70, 30, 30), "" + health);
+        // GUI.Label(new Rect (pos.x, Screen.height-pos.y-70, 30, 30), "" + health);
     }
 }
