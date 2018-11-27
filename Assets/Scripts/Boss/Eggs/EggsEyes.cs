a083ae32933923705d56e9e4bae5f727529dd0ea
Patrick Kochan <patzila@gmail.com>
2018-11-27 12:07:24 -0500

    Initial push for the boss, still need to add attacking functionality

---
+++ Assets/Scripts/Boss/Eggs/EggsEyes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggsEyes : MonoBehaviour {
+
+	public GameObject character = null;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter2D(Collider2D col) {
+		if(col.tag == "Character") {
+			character = col.gameObject;
+		}
+	}
+
+	public void OnTriggerExit2D(Collider2D col) {
+		if(col.tag == "Character") {
+			character = col.gameObject;
+		}
+
+	}
+}
