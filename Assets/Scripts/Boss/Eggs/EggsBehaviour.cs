0a83980595757e18664dfddd8ed3b4f995cceed8
patzila <patzila@gmail.com>
2018-11-27 18:03:52 -0500

    Initial push for the boss, still need to add attacking functionality (#40)

---
+++ Assets/Scripts/Boss/Eggs/EggsBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggsBehaviour : MonoBehaviour
+{
+
+    private int attackType;
+    private float minAttackTime;
+    private float maxAttackTime;
+    private bool attacking = false;
+
+    // Use this for initialization
+    void Awake()
+    {
+        float randomTime = Random.Range(minAttackTime, maxAttackTime);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (attacking)
+        {
+            switch (attackType)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        }
+    }
+
+
+}
