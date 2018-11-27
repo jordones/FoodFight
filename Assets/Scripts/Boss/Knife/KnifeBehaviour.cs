a4deb50d8d2e933ab15e10212aefc64f455db3a7
David Buckley <buckley.w.david@gmail.com>
2018-11-27 00:00:29 -0500

    stabby stabby

--- Assets/Scripts/Boss/Knife/KnifeBehaviour.cs
+++ Assets/Scripts/Boss/Knife/KnifeBehaviour.cs
@@ -7,15 +7,33 @@ public class KnifeBehaviour : MonoBehaviour {
 	public KnifeEyes eyes = null;
 	public int radius = 10;
 
+	public int minStabTime = 10;
+	public int maxStabTime = 20;
+
 	private float t = 0;
 
 	private static float dt = 1f;
 	private bool stabbing = false;
+
+
 	
 
 	// Use this for initialization
 	void Awake () {
-		
+		float randomTime = Random.Range(minStabTime, maxStabTime);
+		Invoke("StabAttack", randomTime);
+	}
+
+	bool CanSeeCharacter() {
+		return eyes != null && eyes.active;
+	}
+
+	
+	void StabAttack()
+	{
+		float randomTime = Random.Range(minStabTime, maxStabTime);
+		StartStab();
+		Invoke("StabAttack", randomTime);
 	}
 	
 	void OnTriggerEnter2D(Collider2D col) {
@@ -30,9 +48,17 @@ public class KnifeBehaviour : MonoBehaviour {
         }
     }
 
+	void StartStab() {
+		if (CanSeeCharacter()) {
+			Debug.Log("Starting to stab");
+			stabbing = true;
+			t = (t + Mathf.PI) % (2*Mathf.PI);
+		}
+	}
+
     // Update is called once per frame
     void FixedUpdate () {
-		if (eyes != null && eyes.active) {
+		if (CanSeeCharacter()) {
 			if (!stabbing) {
 				Transform target = eyes.character.transform;
 
@@ -57,6 +83,27 @@ public class KnifeBehaviour : MonoBehaviour {
 				t = t % (2*Mathf.PI);
 			} else {
 				// Stab towards character
+				Transform target = eyes.character.transform;
+
+				float dx = radius*Mathf.Cos(t);
+				float dy = radius*Mathf.Sin(t);
+
+				Vector3 delta = new Vector3(dx, dy, 0);
+				// The step size is equal to speed times frame time.
+
+				float speed = 20;
+				float step = speed * Time.deltaTime;
+
+        		// Move our position a step closer to the target.
+				transform.position = Vector3.MoveTowards(transform.position, target.position + delta, step);
+				float distance = Vector3.Distance(transform.position, target.position + delta);
+
+				Debug.Log("" + distance);
+
+				if (distance < 0.1) {
+					Debug.Log("Done stabbing");
+					stabbing = false;
+				}
 			}
 		}
 	}
