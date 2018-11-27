d7f4a0402b1a122bdde74f4ae056f870d0179076
School <patzila@gmail.com>
2018-11-26 19:20:32 -0500

    Initial end screen

---
+++ Assets/Scripts/RunCompleted.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunCompleted : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        foreach (Transform itemStand in transform)
+        {
+            // unlock 3 items
+
+        }
+    }
+
+}
