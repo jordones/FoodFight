5df87e0a2efc40ccea971ec716866297eb4faf94
Nikolas Orkic <niorkic@gmail.com>
2018-11-27 01:25:38 -0500

    Addin Pineapple Boss

---
+++ Assets/Scripts/Boss/Pineapple/PineappleEyes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PineappleEyes : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject character = null;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnTriggerEnter2D(Collider2D col) {
+		if(col.tag == "Character") {
+			character = col.gameObject;
+		}
+	}
+
+	public void OnTriggerExit2D(Collider2D col) {
+		if(col.tag == "Character") {
+			character = col.gameObject;
+		}
+
+	}
+}
